5de39f1c71cec2e5a1f4ea63e8b78096ad70d7b6
Kate McElroy <katemcelroy@outlook.com>
2023-01-14 15:36:51 -0800

    added footer

--- pages/posts/masterlist.js
+++ pages/posts/masterlist.js
@@ -29,7 +29,7 @@ const csvFetcher = url => fetch(url)
       html.push(
       <div className="albumHeader">
         <p className="albumTitle">{pcs[0][2]}</p>
-        <p className="albumVersion">{pcs[0][3]}</p>
+        <p className="albumVersion">{pcs[0][3].replaceAll(",", "/")}</p>
       </div>);
       for (const [i, pc] of Object.entries(pcs)) {
         const type = pc[0];
@@ -73,7 +73,6 @@ function FirstPost(props) {
         <title>Masterlist</title>
       </Head>
       <h1>Photocard Masterlist</h1>
-      <h2>
         <p><Link href="/">Home</Link></p>
         <div class="search-wrapper">
             <label for="search">Search Photocards</label>
@@ -81,8 +80,14 @@ function FirstPost(props) {
         </div>
         <div className="container">
           {data}
-        </div>
-      </h2>
+      <footer>
+        <p><i>Links</i></p>
+        <p><a href="https://discord.gg/bB45cxwXvn">Discord</a></p>
+        <p><a href="https://twitter.com/btsflipthru">Twitter</a></p>
+        <p><a href="https://www.instagram.com/btsflipthru/">Instagram</a></p>
+        <p><a href="mailto:btsvirtualbinder@gmail.com">Email</a></p>
+      </footer>
+      </div>
     </>
   );
 }
